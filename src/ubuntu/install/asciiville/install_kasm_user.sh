9ea650567203821b60bdef9ef85009c0b1fae90b
Ronald Record <github@ronrecord.com>
2023-10-31 21:34:57 -0700

    Start on Asciiville

---
+++ src/ubuntu/install/asciiville/install_kasm_user.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPT_PATH="$(realpath $SCRIPT_PATH)"
+umask 022
+
+[ -d $HOME ] || mkdir -p $HOME
+chmod 755 $HOME
+cp ${SCRIPT_PATH}/kasm-user/.aliases $HOME
+cp ${SCRIPT_PATH}/kasm-user/.bashrc $HOME
+chmod 644 $HOME/.aliases $HOME/.bashrc
+
+if [ -d $HOME/.config ]; then
+  if [ -d $HOME/.config/autostart ]; then
+    cp ${SCRIPT_PATH}/kasm-user/.config/autostart/lazyman.desktop $HOME/.config/autostart
+  else
+    cp -a ${SCRIPT_PATH}/kasm-user/.config/autostart $HOME/.config
+  fi
+  if [ -d $HOME/.config/xfce4 ]; then
+    tar cf - -C ${SCRIPT_PATH}/kasm-user/.config xfce4 | tar xf - -C $HOME/.config
+  else
+    cp -a ${SCRIPT_PATH}/kasm-user/.config/xfce4 $HOME/.config
+  fi
+else
+  cp -a ${SCRIPT_PATH}/kasm-user/.config $HOME
+fi
+find $HOME/.config -type f | xargs chmod 644
+find $HOME/.config -type d | xargs chmod 755
+
+if [ -d $HOME/.local ]; then
+  if [ -d $HOME/.local/bin ]; then
+    cp ${SCRIPT_PATH}/kasm-user/.local/bin/lazyman $HOME/.local/bin
+  else
+    cp -a ${SCRIPT_PATH}/kasm-user/.local/bin $HOME/.local
+  fi
+  if [ -d $HOME/.local/share ]; then
+    tar cf - -C ${SCRIPT_PATH}/kasm-user/.local share | tar xf - -C $HOME/.local
+  else
+    cp -a ${SCRIPT_PATH}/kasm-user/.local/share $HOME/.local
+  fi
+else
+  cp -a ${SCRIPT_PATH}/kasm-user/.local $HOME
+fi
+find $HOME/.local -type d | xargs chmod 755
+
+if [ -d $HOME/Desktop ]; then
+  cp ${SCRIPT_PATH}/kasm-user/Desktop/* $HOME/Desktop
+else
+  cp -a ${SCRIPT_PATH}/kasm-user/Desktop $HOME
+fi
+chmod 755 $HOME/Desktop
+
+if [ -d $HOME/bin ]; then
+  cp ${SCRIPT_PATH}/kasm-user/bin/* $HOME/bin
+else
+  cp -a ${SCRIPT_PATH}/kasm-user/bin $HOME
+fi
+chmod 755 $HOME/bin
+
+if [ -d $HOME/.cargo ]; then
+  cp ${SCRIPT_PATH}/kasm-user/.cargo/env $HOME/.cargo
+else
+  cp -a ${SCRIPT_PATH}/kasm-user/.cargo $HOME
+fi
+chmod 755 $HOME/.cargo
+
+if [ -d $HOME/logs ]; then
+  cp ${SCRIPT_PATH}/kasm-user/logs/* $HOME/logs
+else
+  cp -a ${SCRIPT_PATH}/kasm-user/logs $HOME
+fi
+chmod 755 $HOME/logs
+
+chown -R 1000:1000 $HOME/
---
+++ src/ubuntu/install/asciiville/install_tools_asciiville.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+apt update
+apt install -y jq
+apt install -y fzf
+apt install -y ripgrep
+apt install -y bat
+apt install -y lsd
+apt install -y figlet
+apt install -y lolcat
+apt install -y xclip
+apt install -y xsel
+apt install -y python3
+apt install -y python3-venv
+apt install -y uuid-runtime
+apt install -y libaa-bin
+apt install -y libaa1
+apt install -y bb
+apt install -y dconf-cli
+apt install -y libncurses-dev
+apt install -y libjpeg-dev
+apt install -y libpng-dev
+apt install -y khard
+apt install -y git
+apt install -y mplayer
+apt install -y libportaudio2
+apt install -y libportaudiocpp0
+apt install -y portaudio19-dev
+apt install -y wl-clipboard
