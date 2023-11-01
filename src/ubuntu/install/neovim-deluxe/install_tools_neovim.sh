0ae557406d8e89dc9ba3f881f9ba3a16c03e1817
Ronald Record <github@ronrecord.com>
2023-10-31 23:07:25 -0700

    Add kasm-user config for neovim deluxe

---
+++ src/ubuntu/install/neovim-deluxe/install_tools_neovim.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+apt update
+apt install -y jq
+apt install -y fzf
+apt install -y ripgrep
+apt install -y luarocks
+apt install -y julia
+apt install -y php
+apt install -y composer
+apt install -y ccls
+apt install -y bat
+apt install -y lsd
+apt install -y figlet
+apt install -y luarocks
+apt install -y lolcat
+apt install -y xclip
+apt install -y xsel
+apt install -y python3
+apt install -y python3-venv
+apt install -y ruby
+apt install -y ruby-dev
+apt install -y wl-clipboard
