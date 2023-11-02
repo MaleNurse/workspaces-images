9dca558e946cfe895daba83382c11927e600b5eb
Ronald Record <github@ronrecord.com>
2023-11-02 12:48:28 -0700

    Sync up deluxe desktops

--- src/ubuntu/install/install_kasm_user.sh
+++ src/ubuntu/install/install_kasm_user.sh
@@ -8,74 +8,109 @@ umask 022
 
 [ -d $HOME ] || mkdir -p $HOME
 chmod 755 $HOME
-cp ${SCRIPT_PATH}/kasm-user/.aliases $HOME
-cp ${SCRIPT_PATH}/kasm-user/.bashrc $HOME
-chmod 644 $HOME/.aliases $HOME/.bashrc
+[ -f ${SCRIPT_PATH}/kasm-user/.aliases ] && {
+  cp ${SCRIPT_PATH}/kasm-user/.aliases $HOME
+}
+[ -f ${SCRIPT_PATH}/kasm-user/.bashrc ] && {
+  cp ${SCRIPT_PATH}/kasm-user/.bashrc $HOME
+}
+[ -f $HOME/.aliases ] && chmod 644 $HOME/.aliases
+[ -f $HOME/.bashrc ] && chmod 644 $HOME/.bashrc
 
 if [ -d $HOME/.config ]; then
   if [ -d $HOME/.config/autostart ]; then
-    cp ${SCRIPT_PATH}/kasm-user/.config/autostart/lazyman.desktop $HOME/.config/autostart
+    [ -d ${SCRIPT_PATH}/kasm-user/.config/autostart ] && {
+      for autodesk in ${SCRIPT_PATH}/kasm-user/.config/autostart/*
+      do
+        [ "${autodesk}" == "${SCRIPT_PATH}/kasm-user/.config/autostart/*" ] && continue
+        cp ${autodesk} $HOME/.config/autostart
+      done
+    }
   else
-    cp -a ${SCRIPT_PATH}/kasm-user/.config/autostart $HOME/.config
+    [ -d ${SCRIPT_PATH}/kasm-user/.config/autostart ] && {
+      cp -a ${SCRIPT_PATH}/kasm-user/.config/autostart $HOME/.config
+    }
   fi
-  if [ -d $HOME/.config/kitty ]; then
-    tar cf - -C ${SCRIPT_PATH}/kasm-user/.config kitty | tar xf - -C $HOME/.config
-  else
-    cp -a ${SCRIPT_PATH}/kasm-user/.config/kitty $HOME/.config
-  fi
-  if [ -d $HOME/.config/xfce4 ]; then
-    tar cf - -C ${SCRIPT_PATH}/kasm-user/.config xfce4 | tar xf - -C $HOME/.config
-  else
-    cp -a ${SCRIPT_PATH}/kasm-user/.config/xfce4 $HOME/.config
-  fi
+  [ -d ${SCRIPT_PATH}/kasm-user/.config/kitty ] && {
+    if [ -d $HOME/.config/kitty ]; then
+      tar cf - -C ${SCRIPT_PATH}/kasm-user/.config kitty | tar xf - -C $HOME/.config
+    else
+      cp -a ${SCRIPT_PATH}/kasm-user/.config/kitty $HOME/.config
+    fi
+  }
+  [ -d ${SCRIPT_PATH}/kasm-user/.config/xfce4 ] && {
+    if [ -d $HOME/.config/xfce4 ]; then
+      tar cf - -C ${SCRIPT_PATH}/kasm-user/.config xfce4 | tar xf - -C $HOME/.config
+    else
+      cp -a ${SCRIPT_PATH}/kasm-user/.config/xfce4 $HOME/.config
+    fi
+  }
 else
-  cp -a ${SCRIPT_PATH}/kasm-user/.config $HOME
+  [ -d ${SCRIPT_PATH}/kasm-user/.config ] && {
+    cp -a ${SCRIPT_PATH}/kasm-user/.config $HOME
+  }
 fi
 find $HOME/.config -type f | xargs chmod 644
 find $HOME/.config -type d | xargs chmod 755
 
 if [ -d $HOME/.local ]; then
   if [ -d $HOME/.local/bin ]; then
-    cp ${SCRIPT_PATH}/kasm-user/.local/bin/lazyman $HOME/.local/bin
+    [ -f ${SCRIPT_PATH}/kasm-user/.local/bin/lazyman ] && {
+      cp ${SCRIPT_PATH}/kasm-user/.local/bin/lazyman $HOME/.local/bin
+    }
   else
-    cp -a ${SCRIPT_PATH}/kasm-user/.local/bin $HOME/.local
+    [ -d ${SCRIPT_PATH}/kasm-user/.local/bin ] && {
+      cp -a ${SCRIPT_PATH}/kasm-user/.local/bin $HOME/.local
+    }
   fi
-  if [ -d $HOME/.local/share ]; then
-    tar cf - -C ${SCRIPT_PATH}/kasm-user/.local share | tar xf - -C $HOME/.local
-  else
-    cp -a ${SCRIPT_PATH}/kasm-user/.local/share $HOME/.local
-  fi
+  [ -d ${SCRIPT_PATH}/kasm-user/.local/share ] && {
+    if [ -d $HOME/.local/share ]; then
+      tar cf - -C ${SCRIPT_PATH}/kasm-user/.local share | tar xf - -C $HOME/.local
+    else
+      cp -a ${SCRIPT_PATH}/kasm-user/.local/share $HOME/.local
+    fi
+  }
 else
-  cp -a ${SCRIPT_PATH}/kasm-user/.local $HOME
+  [ -d ${SCRIPT_PATH}/kasm-user/.local ] && {
+    cp -a ${SCRIPT_PATH}/kasm-user/.local $HOME
+  }
 fi
 find $HOME/.local -type d | xargs chmod 755
 
-if [ -d $HOME/Desktop ]; then
-  cp ${SCRIPT_PATH}/kasm-user/Desktop/* $HOME/Desktop
-else
-  cp -a ${SCRIPT_PATH}/kasm-user/Desktop $HOME
-fi
+[ -d ${SCRIPT_PATH}/kasm-user/Desktop ] && {
+  if [ -d $HOME/Desktop ]; then
+    cp ${SCRIPT_PATH}/kasm-user/Desktop/* $HOME/Desktop
+  else
+    cp -a ${SCRIPT_PATH}/kasm-user/Desktop $HOME
+  fi
+}
 chmod 755 $HOME/Desktop
 
-if [ -d $HOME/bin ]; then
-  cp ${SCRIPT_PATH}/kasm-user/bin/* $HOME/bin
-else
-  cp -a ${SCRIPT_PATH}/kasm-user/bin $HOME
-fi
-chmod 755 $HOME/bin
+[ -d ${SCRIPT_PATH}/kasm-user/bin ] && {
+  if [ -d $HOME/bin ]; then
+    cp ${SCRIPT_PATH}/kasm-user/bin/* $HOME/bin
+  else
+    cp -a ${SCRIPT_PATH}/kasm-user/bin $HOME
+  fi
+}
+[ -d $HOME/bin ] && chmod 755 $HOME/bin
 
-if [ -d $HOME/.cargo ]; then
-  cp ${SCRIPT_PATH}/kasm-user/.cargo/env $HOME/.cargo
-else
-  cp -a ${SCRIPT_PATH}/kasm-user/.cargo $HOME
-fi
-chmod 755 $HOME/.cargo
+[ -d ${SCRIPT_PATH}/kasm-user/.cargo ] && {
+  if [ -d $HOME/.cargo ]; then
+    cp ${SCRIPT_PATH}/kasm-user/.cargo/env $HOME/.cargo
+  else
+    cp -a ${SCRIPT_PATH}/kasm-user/.cargo $HOME
+  fi
+}
+[ -d $HOME/.cargo ] && chmod 755 $HOME/.cargo
 
-if [ -d $HOME/logs ]; then
-  cp ${SCRIPT_PATH}/kasm-user/logs/* $HOME/logs
-else
-  cp -a ${SCRIPT_PATH}/kasm-user/logs $HOME
-fi
-chmod 755 $HOME/logs
+[ -d ${SCRIPT_PATH}/kasm-user/logs ] && {
+  if [ -d $HOME/logs ]; then
+    cp ${SCRIPT_PATH}/kasm-user/logs/* $HOME/logs
+  else
+    cp -a ${SCRIPT_PATH}/kasm-user/logs $HOME
+  fi
+}
+[ -d $HOME/logs ] && chmod 755 $HOME/logs
 
 chown -R 1000:1000 $HOME/
