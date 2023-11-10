243a29fd2f760768ad966cd52c7974b24ed150c8
Ronald Record <github@ronrecord.com>
2023-11-10 15:54:03 -0800

    Add build/push scripts and Dockerfile for Ubuntu Jammy desktop

---
+++ src/ubuntu/install/jammy/install_tools_jammy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y apt-utils
+apt-get install -y jq
+apt-get install -y fzf
+apt-get install -y g++
+apt-get install -y ripgrep
+apt-get install -y bat
+apt-get install -y lsd
+apt-get install -y figlet
+apt-get install -y lolcat
+apt-get install -y libnotify-bin
+apt-get install -y xclip
+apt-get install -y xsel
+apt-get install -y python3
+apt-get install -y python3-venv
+apt-get install -y ruby
+apt-get install -y ruby-dev
+apt-get install -y wl-clipboard
