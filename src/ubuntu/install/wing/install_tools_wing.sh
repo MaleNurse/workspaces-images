2652a93866908b1324aba6cefd4e42b23485ce22
Ronald Record <github@ronrecord.com>
2023-11-15 13:29:53 -0800

    Install nvm node version manager during postinstall

--- src/ubuntu/install/wing/install_tools_wing.sh
+++ src/ubuntu/install/wing/install_tools_wing.sh
@@ -2,6 +2,9 @@
 
 apt-get update
 apt-get install -y apt-utils
+apt-get install -y ca-certificates
+apt-get install -y curl
+apt-get install -y gnupg
 apt-get install -y jq
 apt-get install -y fzf
 apt-get install -y g++
@@ -18,32 +21,3 @@ apt-get install -y python3-venv
 apt-get install -y ruby
 apt-get install -y ruby-dev
 apt-get install -y wl-clipboard
-
-## Remove the GPG keyring file associated with the old repository
-# rm -f /etc/apt/keyrings/nodesource.gpg
-## Remove the old repository's list file
-# rm -f /etc/apt/sources.list.d/nodesource.list
-
-# Define the desired Node.js major version
-# Currently supported major versions:
-# NODE_MAJOR=16
-# NODE_MAJOR=18
-# NODE_MAJOR=20
-# NODE_MAJOR=21
-
-# Update local package index
-apt-get update
-# Install necessary packages for downloading and verifying new repository information
-apt-get install -y ca-certificates curl gnupg
-## Create a directory for the new repository's keyring, if it doesn't exist
-# mkdir -p /etc/apt/keyrings
-## Download the new repository's GPG key and save it in the keyring directory
-# curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | \
-#   gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-## Add the new repository's source list with its GPG key for package verification
-# echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-
-## Update local package index to recognize the new repository
-# sudo apt-get update
-## Install Node.js from the new repository
-# sudo apt-get install -y nodejs
