7ddf9b2e8ee50d04ce2ef0a47dbfb2d5764ceb36
Ronald Record <github@ronrecord.com>
2024-01-10 08:54:04 -0800

    Add MySQL Workbench install folder

---
+++ src/ubuntu/install/mysql-workbench/install_tools_workbench.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+
+apt-get update
+apt-get install -y gnome-keyring
+apt-get install -y libatkmm-1.6-1v5
+apt-get install -y libcairomm-1.0-1v5
+apt-get install -y libglibmm-2.4-1v5
+apt-get install -y libgtk2.0-0
+apt-get install -y libgtkmm-3.0-1v5
+apt-get install -y libmysqlclient21
+apt-get install -y libopengl0
+apt-get install -y libpangomm-1.4-1v5
+apt-get install -y libpcrecpp0v5
+apt-get install -y libproj22
+apt-get install -y libpython3.10
+apt-get install -y libsecret-1-0
+apt-get install -y libsigc++-2.0-0v5
+apt-get install -y libssh-4
+apt-get install -y libvsqlitepp3v5
+apt-get install -y libzip4
